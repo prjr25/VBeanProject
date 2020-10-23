15517dc8413c510b50320af885203759de8e76f9
Patrick <prjr25@gmail.com>
2020-10-22 18:48:06 -0700

    React Configuration

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+
+class App extends React.Component {
+    render( {
+      return (
+        
+      )
+    })
+}
+
+export default App;
