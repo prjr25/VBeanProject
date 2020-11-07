f3b07a3eec15af391b4a90e30109b84b8a0bf2c8
Patrick <prjr25@gmail.com>
2020-11-07 12:08:58 -0800

    menu rework

---
+++ src/shared/menus.js
@@ -0,0 +1,134 @@
+export const MENUS = [
+    {
+        value: 0,
+        name: beverages,
+        menu: [
+            {
+                id: 0,
+                name: "Black Coffee",
+                size: "small",
+                price: 1.75
+            },
+            {
+                id: 1,
+                name: "Black Coffee",
+                size: "large",
+                price: 2.50
+            },
+            {
+                id: 2,
+                name: "Latte",
+                size: "small",
+                price: 2.75
+            },
+            {
+                id: 3,
+                name: "Latte",
+                size: "large",
+                price: 3.25
+            },
+            {
+                id: 4,
+                name: "Americano",
+                size: "small",
+                price: 2.50
+            },
+            {
+                id: 5,
+                name: "Americano",
+                size: "large",
+                price: 3.00
+            },
+            {
+                id: 6,
+                name: "Iced Coffee",
+                size: "small",
+                price: 2.00
+            },
+            {
+                id: 7,
+                name: "Iced Coffee",
+                size: "large",
+                price: 2.75
+            }
+        ]
+    },
+    {
+        value: 1,
+        name: breakfast,
+        menu: [
+            {
+                id: 0,
+                name: "Scrambled Tofu",
+                description: "Tofu scrambled with onions, tomatoes, and spinach.",
+                price: 7.95
+            },
+            {
+                id: 1,
+                name: "Pancakes",
+                description: "Stack of fluffy pancakes topped with fresh fruit. Served with warm syrup.",
+                price: 8.95
+            },
+            {
+                id: 2,
+                name: "French Toast",
+                description: "Fresh cinnamon french toast. Served with warm syrup.",
+                price: 8.95
+            },
+            {
+                id: 3,
+                name: "Biscuits 'n Gravy",
+                description: "Fresh baked biscuits with warm mushroom gravy.",
+                price: 9.95
+            },
+            {
+                id: 4,
+                name: "Tempeh Scrambler",
+                description: "Tempeh scrambled with seasoned potatoes, onions, and peppers. Served with Toast.",
+                price: 10.95
+            }
+        ]
+    },
+    {
+        value: 2,
+        name: dinner,
+        menu: [
+            {
+                id: 0,
+                name: "Garden Salad",
+                description: "Romaine lettuce with onions, carrots, tomatoes, and chickpeas.",
+                price: 6.95
+            },
+            {
+                id: 1,
+                name: "Coconut Curry Soup",
+                description: "Fresh veggies slow cooked in coconut milk and curry spices.",
+                price: 7.95
+            },
+            {
+                id: 2,
+                name: "Sheppard's Pie",
+                description: "Mixed vegetables and quinoa in between layers of creamy mashed potatoes.",
+                price: 8.95
+            },
+            {
+                id: 3,
+                name: "Pile-High Nachos",
+                description: "Huge serving of tortilla chips topped with cashew cheese, black beans, and green onion.",
+                price: 9.95
+            },
+            {
+                id: 4,
+                name: "Philly Tempeh Sub",
+                description: "Tempeh sauted with onions and peppers on homemade bread. Served with fries.",
+                price: 10.95
+            },
+            {
+                id: 5,
+                name: "Black Bean Burger",
+                description: "Homemade black bean patty with onions, lettuce, and tomato on a sesame seed bun. Served with fries.",
+                price: 10.95
+            }
+        ]
+    }
+]
