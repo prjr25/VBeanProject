d515579460d8c0b486f5afd26cd21a84264b5012
Patrick <prjr25@gmail.com>
2020-11-11 08:17:54 -0800

    Modal added, needs work

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import  Logo from './Logo';
-import { Jumbotron, Nav, Navbar, NavbarToggler, Collapse, NavItem, Button } from 'reactstrap';
+import { Jumbotron, Nav, Navbar, NavbarToggler, Collapse, NavItem, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 
@@ -13,12 +13,19 @@ class Header extends React.Component{
         };
 
         this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
 
     }
 
     toggleNav() {
         this.setState({
             isNavOpen: !this.state.isNavOpen
+        });
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
         });
     }
 
@@ -70,13 +77,46 @@ class Header extends React.Component{
                                     </NavItem>
                                 </Nav>
                                 <span className="ml-auto">
-                                    <Button>
+                                    <Button onClick={this.toggleModal}>
                                         <i className="fa fa-sign-in" /> Member Sign In
                                     </Button>
                                 </span>                            
                         </Collapse>
                     </div>
                 </Navbar>
+
+                <Modal isOpen={this.state.isModalOpen} toggle={this.state.toggleModal}>
+                    <ModalHeader>
+                        <h3>Member Sign In</h3>
+                        <Button type="button" className="ml-auto" onClick={this.toggleModal}>
+                        <span aria-hidden="true">&times;</span>
+                        </Button>
+                    </ModalHeader>
+                        <ModalBody>
+                            <Form>
+                                <FormGroup>
+                                    <Label htmlFor="email">Email:</Label>
+                                    <Input type="text" id="email" name="email" />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label htmlFor="password">Password:</Label>
+                                    <Input type="text" id="password" name="password" />
+                                </FormGroup>
+                                <FormGroup check>
+                                    <Label check>
+                                        <Input type="checkbox" name="remember" />
+                                        Remember Me
+                                    </Label> 
+                                    <span>                               
+                                        <Button type="button" className="btn btn-secondary btn-sm m-2" onClick={this.toggleModal}>Cancel</Button>
+                                        <Button type="submit" className="btn btn-success btn-sm m-2">Sign In</Button>
+                                    </span>
+                                </FormGroup>
+                            </Form>
+                        </ModalBody>
+
+                </Modal>
+
 
             </React.Fragment>
         );
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Logo extends React.Component {
     render() {
         return (
             <div>
-                <img src="https://res.cloudinary.com/prjr25/image/upload/v1604631219/VBeanProject/images/logo/vBeanLogo_pbkqau.png" />
+                <Link to='/home'><img src="https://res.cloudinary.com/prjr25/image/upload/v1604631219/VBeanProject/images/logo/vBeanLogo_pbkqau.png" alt="V Bean Logo" /></Link>
             </div>
         )
     }
